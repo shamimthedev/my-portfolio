12a7895ee59776bb6e4ad0f34bbad38bdf7e547c
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-08 01:47:41 +0600

    (feat) Projects Section Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "@/sections/Header";
 import HeroSection from "@/sections/HeroSection";
+import { ProjectsSection } from "@/sections/Projects";
 
 export default function Home() {
   return (
     <>
       <Header/>
       <HeroSection/>
+      <ProjectsSection/>
     </>
   );
 }
