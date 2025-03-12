41ca2ca9a651c1edc8b32e1395925af02caed768
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-03-12 23:02:41 +0600

    Updated

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -71,7 +71,7 @@ export const HeroSection = () => {
           </h1>
           <p className="mt-4 text-center text-white/60 md:text-lg">
             I specialize in transforming designs into functional,
-            high-performing web applications. Let's discuss your next project.
+            high-performing web applications. Let&apos;s discuss your next project.
           </p>
         </div>
         <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
@@ -82,7 +82,7 @@ export const HeroSection = () => {
           </button>
           <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 px-5 h-12 rounded-xl">
             <span>👏</span>
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
           </button>
         </div>
       </div>
