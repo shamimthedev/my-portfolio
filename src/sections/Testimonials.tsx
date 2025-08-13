c0817bc282310a0e84bb3c53739d715a01efa7fc
Md. Shamim Hossain <smdshamimhossain675@gmail.com>
2025-08-14 01:06:53 +0600

    update

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,43 +1,43 @@
-import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
-import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
-import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
-import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
 import SectionHeader from "@/components/SectionHeader";
 import Image from "next/image";
 import { Card } from "@/components/Card";
 import { Fragment } from "react";
+import avatar1 from "@/assets/images/memoji-avatar-1.png"
+import avatar2 from "@/assets/images/memoji-avatar-2.png"
+import avatar3 from "@/assets/images/memoji-avatar-3.png"
+import avatar4 from "@/assets/images/memoji-avatar-4.png"
+import avatar5 from "@/assets/images/memoji-avatar-5.png"
 
 const testimonials = [
   {
     name: "Elham Khan",
     position: "Marketing Manager @NestMart",
     text: "Shamim was instrumental in transforming our website into a powerful marketing tool. His attention to detail and ability to understand our brand is exceptional. We're thrilled with the results!",
-    avatar: memojiAvatar1,
+    avatar: avatar1,
   },
   {
     name: "Habiba Amir",
     position: "Head of Design @eMoogli",
     text: "Working with Shamim was a pleasure. His expertise in frontend development brought our designs to life in a way we never imagined. The website has exceeded our expectations.",
-    avatar: memojiAvatar2,
+    avatar: avatar2,
   },
   {
     name: "Jahidul Bashar",
     position: "CEO @HouseDecor",
     text: "Shamim's ability to create seamless user experiences is unmatched. Our website has seen a significant increase in conversions since launching the new design. We couldn't be happier.",
-    avatar: memojiAvatar3,
+    avatar: avatar3,
   },
   {
     name: "Shaidul Islam",
     position: "Product Manager @NestMart",
     text: "Shamim is a true frontend wizard. He took our complex product and transformed it into an intuitive and engaging user interface. We're already seeing positive feedback from our customers.",
-    avatar: memojiAvatar4,
+    avatar: avatar4,
   },
   {
     name: "Hamim Biswas",
     position: "Director of IT @eMoogli",
     text: "Shamim's work on our website has been nothing short of exceptional. He's a talented developer who is also a great communicator. We highly recommend him.",
-    avatar: memojiAvatar5,
+    avatar: avatar5,
   },
 ];
 
